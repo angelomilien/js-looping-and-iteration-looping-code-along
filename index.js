297cd2a37969866b312576c90cc1216e3040688b
Angelo <angelomilienjoseph@gmail.com>
2021-08-24 17:55:52 -0400

    commit tree e16ecb37b4e92e011ddb6953def8e7b7e2dcf60a

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(nameArray, theOtherWord) {
+    let phrase = []
+    for (let i = 0; i < nameArray.length; i++) {
+        phrase.push(`Thank you, ${nameArray[i]}, for the wonderful ${theOtherWord} gift!`);
+    }
+    return phrase
+}
+
+function countDown(num){
+    while (num >= 0) {
+        console.log(num)
+        num = num - 1
+    }
+}
